cd700a98fe63b85bf5254068d2d9b69fbc54ae8b
fizah <fizah_muharrani@yahoo.com>
2015-02-23 11:59:08 +0700

    Update skor.php
    tambahin proses tambah, dan update

--- application/controllers/skor.php
+++ application/controllers/skor.php
@@ -8,13 +8,14 @@ class skor extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('model_skor', '', TRUE);
+		$this->load->model('skor_model', '', TRUE);
 		$this->load->model('helper_model', '', TRUE);
 	}
 
 	/**
 	 * Inisialisasi variabel untuk $title(untuk id element <body>)
 	 */
-	var $title = 'asesor';
+	var $title = 'skor';
 
 	function index()
 	{
@@ -86,4 +87,141 @@ class skor extends CI_Controller {
 		$this->load->view('template_kegiatan', $data);
 	}
 
+	/**
+	 * Menghapus data skor dengan nomor tertentu
+	 */
+	function delete($id_skor)
+	{
+		$this->model_skor->delete('id_skor',$id_skor,'skor');
+		$this->session->set_flashdata('message', '1 data skor berhasil dihapus');
+
+		redirect('skor');
+	}
+
+	/**
+	 * Menampilkan form tambah skor
+	 */
+	function add()
+	{
+		$data['title'] 			= $this->title;
+		$data['h2_title'] 		= 'Data Skor > Tambah Data';
+		$data['main_view'] 		= 'skor/skor_form';
+		$data['form_action']		= site_url('skor/add_process');
+		$data['link'] 			= array('link_back' => anchor('skor','kembali', array('class' => 'back'))
+										);
+
+		$this->load->view('template', $data);
+	}
+	/**
+	 * Proses tambah data skor
+	 */
+	function add_process()
+	{
+		$data['title'] 			= $this->title;
+		$data['h2_title'] 		= 'Data Skor > Tambah Data';
+		$data['main_view'] 		= 'skor/skor_form';
+		$data['form_action']		= site_url('skor/add_process');
+		$data['link'] 			= array('link_back' => anchor('skor/','kembali', array('class' => 'back'))
+										);
+		$skor = $this->input->post();
+		unset($skor['submit']);
+		unset($skor['id_skor']);
+		$this->helper_model->printr($skor);
+		$this->skor_model->add($skor);
+		$this->session->set_flashdata('message', 'Satu data skor berhasil disimpan!');
+		redirect('skor');
+
+	}
+
+	/**
+	 * Menampilkan form update data skor
+	 */
+	function update($id_skor)
+	{
+		$data['title'] 			= $this->title;
+		$data['h2_title'] 		= 'Data Skor > Update';
+		$data['main_view'] 		= 'skor/skor_form';
+		$data['form_action']		= site_url('skor/update_process');
+		$data['link'] 			= array('link_back' => anchor('skor','kembali', array('class' => 'back'))
+										);
+
+		$skor = $this->model_skor->search('id_skor',$id_skor,'skor');
+		$skor = $skor[0];
+
+		// auto generate
+		$this->session->set_userdata('skor', $skor->id_skor); 
+
+		$data['default']['id_skor'] = $skor->id_skor;
+		$data['default']['nama'] = $skor->nama;
+		$data['default']['prosentasi_skor'] = $skor->prosentasi_skor;
+		$data['default']['id_group_skor'] = $skor->id_group_skor;
+		$this->load->view('template', $data);
+	}
+
+	/**
+	 * Proses update data skor
+	 */
+	function update_process()
+	{
+		$data['title'] 			= $this->title;
+		$data['h2_title'] 		= 'Data Skor > Update';
+		$data['main_view'] 		= 'skor/skor_form';
+		$data['form_action']		= site_url('skor/update_process');
+		$data['link'] 			= array('link_back' => anchor('skor','kembali', array('class' => 'back'))
+										);
+		$skor = $this->input->post();
+		unset($skor['submit']);
+		// unset($skor['id_skor']);
+		$this->helper_model->printr($skor);
+		$this->model_skor->save($skor,'skor');
+		$this->session->set_flashdata('message', 'Satu data skor berhasil diupdate!');
+		redirect('skor');
+
+	}
+
+	/**
+	 * Validasi untuk nomor, agar tidak ada skor dengan Nomor Peserta Sama sama
+	 */
+	function valid_nomor($nomor)
+	{
+		if ($this->skor_model->valid_nomor($nomor) == TRUE)
+		{
+			$this->form_validation->set_message('valid_nomor', "skor dengan Nama Skor $nama sudah terdaftar");
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+
+	// cek apakah valid untuk update?
+	function valid_nomor2()
+	{
+		// cek agar tidak ada nomor ganda, khusus untuk proses update
+		$current_nomor 	= $this->session->userdata('id_skor');
+		$new_nomor	= $this->input->post('id_skor');
+
+		if ($new_nomor === $current_nomor)
+		{
+			return TRUE;
+		}
+		else
+		{
+			if($this->skor_model->valid_nomor($new_id) === TRUE) // cek database untuk entry yang sama memakai valid_entry()
+			{
+				$this->form_validation->set_message('valid_nomor2', "skor dengan nomor $new_id sudah terdaftar");
+				return FALSE;
+			}
+			else
+			{
+				return TRUE;
+			}
+		}
+	}
+
 }
+// END skor Class
+
+/* End of file skor.php */
+/* Location: ./system/application/controllers/s.php */
